7dec1d368b44bfa18b391943696fcf736ee88c40
Guggilla Raj Kumar <lucifergaming1947@gmail.com>
2023-03-26 22:19:52 +0530

    web-server activated

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
 import http from 'http'
 
+//non persistant way of storing data - WITHOUT DATABASE
+const shinobis = [
+    {
+        id:0,
+        name:'Naruto Uzumaki'
+    },
+    {
+        id:1,
+        name:'Sasuke Uchiha'
+    }
+]
+
+//creating web server
 const server = http.createServer((req,res)=>{
-    res.end(JSON.stringify("Hello you request is succesfull"))
+
+    console.log(req)
+
+    
+    //customising response
+    res.writeHead(200,"success",{ 'Content-Type':'application/json'})
+
+    //adding response data in STANDARD DATA FORMAT - JSON
+    //and sending response and ending request call
+    res.end(JSON.stringify(shinobis))
 })
 
+//activating web server
 server.listen(3000,()=>console.log("web server activated and listening at 3000..."))
